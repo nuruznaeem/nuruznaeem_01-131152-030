71c24a7be96afd8d8f71974dc4f101374a0af94a
nuruz <nuruz.naeem1998@gmail.com>
2018-05-22 13:38:35 -0700

    commit 6

---
+++ serious project/Assets/collision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public class collision : MonoBehaviour
+{
+    public movementplayer move; 
+    void OnCollisionEnter(Collision itsinfo)
+    {
+        if (itsinfo.collider.name == "outcube")
+        {
+            move.enabled = false;
+        }
+
+    }
+}
---
+++ serious project/Assets/followme.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class followme : MonoBehaviour 
+{
+    public Transform myplayer;
+    public Vector3 uncentered;
+    void Update () 
+    {
+        transform.position = myplayer.position + uncentered;
+	}
+
+}
